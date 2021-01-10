6d49ab5bbba25fbd729a62667ba5cf734b580be4
Will Utterback <uwill08@gmail.com>
2021-01-10 12:57:54 -0600

    doubleSearch now passes the imgURL form value into scanFace which passed the variable faceInfo to gooleSearch. googleSearch uses that value to search the websites we have set up and gives us a list of links

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,20 +1,21 @@
 
-const data = {
-  api_key: "CKjT0AMrUWohOGp31Z91LRwt5wLh9frE",
-  api_secret: "u-ZntJ_4-YXqxAQ7kKiLK5PVsy784IIt",
-  image_url: imgURL,
-  return_landmark: "1",
-  return_attributes: "beauty,age,gender",
-}
 
 function doubleSearch(imgURL) {
+
+  const data = {
+    api_key: "CKjT0AMrUWohOGp31Z91LRwt5wLh9frE",
+    api_secret: "u-ZntJ_4-YXqxAQ7kKiLK5PVsy784IIt",
+    image_url: imgURL,
+    return_landmark: "1",
+    return_attributes: "beauty,age,gender",
+  }
   alert('button clicked');
   console.log(imgURL)
   console.log(data);
 
-  function googleSearch() {
+  function googleSearch(searchVar) {
 
-    var queryURL = "https://www.googleapis.com/customsearch/v1?key=AIzaSyC1f9T-Bn9SvN0xu__sSjW-nTrsl_eqp-o&cx=f70009e920516f93e&q=lotion"
+    var queryURL = "https://www.googleapis.com/customsearch/v1?key=AIzaSyC1f9T-Bn9SvN0xu__sSjW-nTrsl_eqp-o&cx=f70009e920516f93e&q=" + searchVar;
     $.ajax({
       url: queryURL,
       method: "GET"
@@ -30,12 +31,16 @@ function doubleSearch(imgURL) {
       method: "POST",
       data: data,
     }).then(function (response) {
+      var faceInfo = response.faces[0].attributes.age.value;
       console.log(response);
+      console.log(faceInfo)
+      googleSearch(faceInfo);
     })
+
   }
 
   // googleSearch();
-  // scanFace();
+  scanFace();
 }
 
 $("#submitButton").on("click", function () {
