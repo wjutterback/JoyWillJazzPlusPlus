7da39bb5a3bc624a6b09ba073fe4e8ca591ed2f7
Refuge001 <48739878+Refuge001@users.noreply.github.com>
2021-01-11 22:28:35 -0600

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,14 @@
 
+//change query URL to skinanalyze API
 // A file input's value attribute contains a DOMString that represents the path to the selected file(s)
 // $('input')[0].files[0] - path to stored img on webpage
 
-function doubleSearch(htmlsrc, htmlsearch) {
+function doubleSearch(htmlsrc, htmlinput) {
   var fileList = $('input').prop('files'); // the array, not used just as a reminder
   var imgFile = $('input').prop('files')[0];
-  const preview = document.querySelector('img')
-  console.log($('input')[0])
-  console.log($('input')[0].files[0])
+  const preview = document.querySelector('img');
   console.log(fileList);
+  console.log(imgFile);
 
   function googleSearch(searchVar) {
 
@@ -26,53 +26,58 @@ function doubleSearch(htmlsrc, htmlsearch) {
       api_key: "CKjT0AMrUWohOGp31Z91LRwt5wLh9frE",
       api_secret: "u-ZntJ_4-YXqxAQ7kKiLK5PVsy784IIt",
       image_base64: file64,
-      return_landmark: "1",
-      return_attributes: "age,gender,skinstatus", // more options available, emotion, smile, ethnicity etc.
     }
     console.log(data);
-    var queryURL = "https://api-us.faceplusplus.com/facepp/v3/detect?"
+    var queryURL = "https://api-us.faceplusplus.com/facepp/v1/skinanalyze";
     $.ajax({
       url: queryURL,
       method: "POST",
       data: data,
     }).then(function (response) {
-      var faceInfoAge = response.faces[0].attributes.age.value;
-      var faceInfoGender = response.faces[0].attributes.gender.value;
-      var faceInfoSkinAcne = response.faces[0].attributes.skinstatus.acne;
-      var faceInfoSkinDarkCircle = response.faces[0].attributes.skinstatus.dark_circle;
-      var faceInfoSkinHealth = response.faces[0].attributes.skinstatus.health;
       // will need to make a conditional statement for what google searches.
       console.log(response);
-      console.log(faceInfoAge)
-      googleSearch(faceInfoAge);
+      //skin_type values below
+      // 0	oily skin
+      // 1	dry skin
+      // 2	normal skin
+      // 3	mixed skin
+      var faceAcne = response.result.acne.value;
+      var faceAcneConfidence = response.result.acne.confidence;
+      var oilySkin = response.result.skin_type.details[0].value;
+      var drySkin = response.result.skin_type.details[1].value;
+      var normalSkin = response.result.skin_type.details[2].value;
+      var mixedSkin = response.result.skin_type.details[3].value;
+      var darkCircle = response.result.dark_circle.value;
+      var darkCircleConfidence = response.result.dark_circle.confidence;
+      // googleSearch(faceAcne); // if faceAcne.value = 1 {googleSearch(faceAcne)} - something like this
     })
   }
 
-  function scanFaceHTML(link) {
+  function scanFaceHTML() {
     preview.src = $('#imgURL').val();
     const data = {
       api_key: "CKjT0AMrUWohOGp31Z91LRwt5wLh9frE",
       api_secret: "u-ZntJ_4-YXqxAQ7kKiLK5PVsy784IIt",
-      image_url: link,
-      return_landmark: "1",
-      return_attributes: "age,gender,skinstatus", // more options available, emotion, smile, ethnicity etc.
+      image_url: htmlsrc,
     }
     console.log(data);
-    var queryURL = "https://api-us.faceplusplus.com/facepp/v3/detect?"
+    var queryURL = "https://api-us.faceplusplus.com/facepp/v1/skinanalyze";
     $.ajax({
       url: queryURL,
       method: "POST",
       data: data,
     }).then(function (response) {
-      var faceInfoAge = response.faces[0].attributes.age.value;
-      var faceInfoGender = response.faces[0].attributes.gender.value;
-      var faceInfoSkinAcne = response.faces[0].attributes.skinstatus.acne;
-      var faceInfoSkinDarkCircle = response.faces[0].attributes.skinstatus.dark_circle;
-      var faceInfoSkinHealth = response.faces[0].attributes.skinstatus.health;
       // will need to make a conditional statement for what google searches.
       console.log(response);
-      console.log(faceInfoAge)
-      googleSearch(faceInfoAge);
+      var faceAcne = response.result.acne.value;
+      var faceAcneConfidence = response.result.acne.confidence;
+      var oilySkin = response.result.skin_type.details[0].value;
+      var drySkin = response.result.skin_type.details[1].value;
+      var normalSkin = response.result.skin_type.details[2].value;
+      var mixedSkin = response.result.skin_type.details[3].value;
+      var darkCircle = response.result.dark_circle.value;
+      var darkCircleConfidence = response.result.dark_circle.confidence;
+      // googleSearch(faceInfoAge);
     })
 
   }
@@ -88,8 +93,8 @@ function doubleSearch(htmlsrc, htmlsearch) {
     reader.readAsDataURL(imgFile) // Takes the file and converts the data to base64
   }
 
-  if (htmlsearch === true) {
-    scanFaceHTML(htmlsrc);
+  if (htmlinput === true) {
+    scanFaceHTML();
   } else {
     encodeIMG();
   }
