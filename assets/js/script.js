15da4966c74dfa7c1e2cf011afe3aab086aa78cf
Joy <joyumoekpo1@gmail.com>
2021-01-11 21:48:21 -0600

    changed API url

---
+++ assets/js/script.js
@@ -0,0 +1,88 @@
+// A file input's value attribute contains a DOMString that represents the path to the selected file(s)
+// $('input')[0].files[0] - path to stored img on webpage
+
+function doubleSearch(htmlsrc, htmlsearch) {
+  var fileList = $('input').prop('files'); // the array, not used just as a reminder
+  var imgFile = $('input').prop('files')[0];
+  const preview = document.querySelector('img')
+  console.log($('input')[0])
+  console.log($('input')[0].files[0])
+  console.log(fileList);
+
+  function googleSearch(searchVar) {
+
+    var queryURL = "https://www.googleapis.com/customsearch/v1?key=AIzaSyC1f9T-Bn9SvN0xu__sSjW-nTrsl_eqp-o&cx=f70009e920516f93e&q=" + searchVar;
+    $.ajax({
+      url: queryURL,
+      method: "GET"
+    }).then(function (response) {
+      console.log(response);
+    })
+  }
+
+  function scanFace(file64) {
+    const data = {
+      api_key: "CKjT0AMrUWohOGp31Z91LRwt5wLh9frE",
+      api_secret: "u-ZntJ_4-YXqxAQ7kKiLK5PVsy784IIt",
+      image_base64: file64,
+      return_landmark: "1",
+      return_attributes: "age,gender,skinstatus", // more options available, emotion, smile, ethnicity etc.
+    }
+    console.log(data);
+    var queryURL = "https://api-us.faceplusplus.com/facepp/v1/skinanalyze";
+    $.ajax({
+      url: queryURL,
+      method: "POST",
+      data: data,
+    }).then(function (response) {
+      console.log(response);
+      // googleSearch(faceInfoAge);
+    })
+  }
+
+  function scanFaceHTML(link) {
+    preview.src = $('#imgURL').val();
+    const data = {
+      api_key: "CKjT0AMrUWohOGp31Z91LRwt5wLh9frE",
+      api_secret: "u-ZntJ_4-YXqxAQ7kKiLK5PVsy784IIt",
+      image_url: link,
+    }
+    console.log(data);
+    var queryURL = "https://api-us.faceplusplus.com/facepp/v1/skinanalyze";
+    $.ajax({
+      url: queryURL,
+      method: "POST",
+      data: data,
+    }).then(function (response) {
+      console.log(response);
+      // googleSearch(faceInfoAge);
+
+    })
+
+  }
+
+  // when encodeIMG is run it will convert our imgFile variable into base64 and display it to page
+  function encodeIMG() {
+    var reader = new FileReader();
+    reader.onloadend = function () {
+      console.log(reader.result); // base64 conversion result
+      preview.src = reader.result; // displays image on site
+      scanFace(reader.result);
+    }
+    reader.readAsDataURL(imgFile) // Takes the file and converts the data to base64
+  }
+
+  if (htmlsearch === true) {
+    scanFaceHTML(htmlsrc);
+  } else {
+    encodeIMG();
+  }
+}
+//currently uses two buttons - would like to just use one but will require more work
+$("#submitButton").on("click", function () {
+  doubleSearch($('#imgURL').val(), true);
+})
+
+$("#fileSubmit").on("click", function () {
+  doubleSearch();
+})
