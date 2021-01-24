4eeadb594f6fe15a7b5da5a0d8911566fad2df66
Refuge001 <48739878+Refuge001@users.noreply.github.com>
2021-01-24 16:31:19 -0600

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -73,7 +73,6 @@ function doubleSearch(htmlsrc, htmlinput) {
       var darkCircle = response.result.dark_circle.value;
       var darkCircleConfidence = response.result.dark_circle.confidence;
       var warning = response.warning[0];
-      console.log(response);
 
       if (warning === undefined) {
       } else if (warning.includes('improper') === true) {
